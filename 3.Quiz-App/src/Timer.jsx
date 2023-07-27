dd3845db3b061cea400d5b903d951b356b0cac01
Tushar Gaonkar <gaonkar.tushar01@gmail.com>
2023-07-27 21:02:27 +0530

    timer implemented

--- 3.Quiz-App/src/Timer.jsx
+++ 3.Quiz-App/src/Timer.jsx
@@ -3,24 +3,32 @@ import { useEffect } from 'react';
 export default function Timer({ state, dispatch }) {
   useEffect(() => {
     function setTimer() {
-      const timerConfig = { Easy: 0.5, Medium: 1, Hard: 2 };
-      const allotedMinutes = (state.totalQuestions * timerConfig[state.difficulty]) / 2;
+      const timerConfig = { Easy: 0.6, Medium: 1, Hard: 1.5 };
+      const allotedMinutes = Math.floor((state.totalQuestions * timerConfig[state.difficulty]) / 2);
       dispatch({ type: 'setTimer', payload: { minutes: allotedMinutes, seconds: 0 } });
     }
 
     function decrementTimer() {
       dispatch({ type: 'decTimer' });
     }
-    // setTimer();
+    setTimer();
 
     const timerId = setInterval(decrementTimer, 1000);
     return () => clearInterval(timerId); //state cleanup
   }, []); // run only on mount
 
   return state.timer !== null ? (
-    <button className='px-4 py-2 rounded-full bg-slate-400 text-white font-semibold'>
+    <button
+      className={
+        state.timer.minutes < 1
+          ? `bg-red-500 px-4 py-2 rounded-full text-white font-semibold`
+          : state.timer.minutes < 2
+          ? ` bg-orange-500 px-4 py-2 rounded-full text-white font-semibold`
+          : ` bg-green-500 px-4 py-2 rounded-full text-white font-semibold`
+      }
+    >
       🕧{state.timer.minutes < 10 && 0}
-      {state.timer.minutes} : {state.timer.seconds < 10 && 0}
+      {state.timer.minutes}:{state.timer.seconds < 10 && 0}
       {state.timer.seconds}
     </button>
   ) : null;
--- 3.Quiz-App/src/Topics.jsx
+++ 3.Quiz-App/src/Topics.jsx
@@ -1,18 +1,23 @@
 import Difficulty from './Difficulty';
 import Options from './Options';
 
-export default function Topics({ topicList, status, dispatch }) {
+export default function Topics({ topicList, difficulty, totalQuestions, status, dispatch }) {
   return (
     <section className='flex flex-col justify-center items-center space-y-3'>
       <div className='flex justify-center'>
         <fieldset className='flex flex-wrap gap-3'>
           <legend className='sr-only'>Subjects</legend>
           {topicList.map((item) => (
-            <Options key={item} subject={item} dispatch={dispatch} />
+            <Options status={status} key={item} subject={item} dispatch={dispatch} />
           ))}
         </fieldset>
       </div>
-      <Difficulty status={status} dispatch={dispatch} />
+      <Difficulty
+        status={status}
+        difficulty={difficulty}
+        totalQuestions={totalQuestions}
+        dispatch={dispatch}
+      />
     </section>
   );
 }
